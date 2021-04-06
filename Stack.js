02230d8a00d17ea254922344b68384ea75cbb04d
mariiamel <mariiamelnikova96@gmail.com>
2021-04-06 00:00:54 -0700

    hw stack and queue

--- Stack.js
+++ Stack.js
@@ -1,1 +1,56 @@
-// Your stack class goes here
+class Stack{
+    constructor()
+    {
+        this.data = []
+    }
+
+    size()
+    {
+        return this.data.length
+    }
+
+    isEmpty()
+    {
+        if (this.data.length == 0) {
+            return true
+        }else {
+            return false
+        }
+    }
+
+    push(element)
+    {
+        this.data.push(element)
+    }
+
+    pop()
+    {
+        if (this.data.length == 0){
+            return
+        } else {
+            this.data.pop()
+        }
+    }
+
+    peek()
+    {
+        return this.data[this.data.length - 1]
+    }
+
+    print()
+    {
+        this.data.forEach(item => {
+            console.log(item)
+        })
+    }
+}
+
+const myStack = new Stack()
+
+myStack.push('new stack')
+myStack.push('the other one')
+myStack.pop()
+myStack.print()
+
+
+
