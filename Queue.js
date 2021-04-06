a3b98c51a1b585a4d8734d2ef22c99c51450134f
ChrisB007 <chrisbitoy@gmail.com>
2021-04-06 12:19:37 -0400

    hw

--- Queue.js
+++ Queue.js
@@ -1,1 +1,43 @@
 // Your Queue Class goes here
+// Your stack class goes here
+class Que {
+    constructor() {
+        this.data  = []
+    }
+
+    size() {
+
+        return this.data.length
+
+    }
+
+    isEmpty() {
+        return this.size() == 0
+
+    }
+
+    push(item){
+        this.data.push(item)
+        return this.size()
+
+    }
+
+    pop(){
+        this.data.pop()
+        this.size()
+
+    }
+
+    peek(){
+        return this.data[this.size() -1]
+
+    }
+
+    print(){
+        this.data.map((item) => {
+            console.log(item)
+        })
+
+    }
+
+}
