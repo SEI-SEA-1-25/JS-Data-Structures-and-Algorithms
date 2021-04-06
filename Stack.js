a2af0e2a36120e74436d137569c48e389e44da9c
dkzerfu <dkzerfu@gmail.com>
2021-04-06 12:36:04 -0400

    homeWork

--- Stack.js
+++ Stack.js
@@ -1,1 +1,34 @@
 // Your stack class goes here
+class Stack{
+    constructor(){
+        this.data = [];
+    }
+
+    size(){
+        return this.data.length
+    }
+    
+    isEmpty(){
+        return this.size() == 0
+    }
+    
+    push(item){
+     this.data.push(item)
+     return this.size()
+    }
+    
+    pop(){
+        this.data.pop()
+        return this.size()
+    }
+    
+    peak(){
+        return this.dada(this.size() - 1)
+    }
+    
+    print(){
+        this.data.forEach(element => {
+            console.log(element)
+        })
+     }
+}
