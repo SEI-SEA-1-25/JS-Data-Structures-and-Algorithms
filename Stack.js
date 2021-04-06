876aee3ab41824b7589a02d21cfbdb22b2d4ebf2
wonkatin <j.drawers@gmail.com>
2021-04-05 22:44:19 -0500

    homework

--- Stack.js
+++ Stack.js
@@ -1,1 +1,57 @@
 // Your stack class goes here
+class Stack {
+    constructor() {
+        this.data = []
+    }
+    size(){
+        return this.data.length
+    }
+    isEmpty() {
+        return this.data.length == 0
+    }
+    push(item) {
+        this.data.push(item)
+        return this.data.length
+    }
+    pop() {
+        if(this.data.length == 0)  {
+            return
+        } else {
+            this.data.pop();
+            // console.log(this.data.length)
+            return this.data
+        }
+    }
+    peek() {
+        return this.data[this.data.length - 1]
+    }
+    print() {
+        let string = '';
+        for(let i = 0; i < this.data.length; i++) {
+            string += this.data[i] + " "
+        }
+        return string
+        // console.log(string)
+    }
+}
+
+
+// ### TEST SUITE ###
+// # Use the following code to help you implement the Stack
+my_stack = new Stack()
+
+console.log('Is Stack empty? Should be True: \n', my_stack.isEmpty())
+
+my_stack.push('A')
+my_stack.push('B')
+my_stack.push('C')
+my_stack.push('D')
+console.log('Is Stack empty? Should be True: \n', my_stack.isEmpty())
+console.log('Popped item should be D: \n', my_stack.pop())
+console.log('Top item should be C: \n', my_stack.peek())
+console.log('Here are all the items in the stack - Should be A B C: \n', my_stack.print())
+console.log('The size of stack should be 3: \n', my_stack.size())
+
+// # Bonus Question - How would our Stack implementation change if we
+// # created it with a LinkedList instead of a List?
+// # What is a LinkedList?
