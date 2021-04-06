4b65d2a6423d6f01f9ba09bbc5a4282aa3e4cff2
SunilDeveloper7 <sunnil474@gmail.com>
2021-04-06 09:27:29 -0700

    solution for JS-data-structure and algo

--- Queue.js
+++ Queue.js
@@ -1,1 +1,42 @@
 // Your Queue Class goes here
+
+class Queue {
+    constructor() {
+      this.data = []
+    }
+  
+    // returns the size of the queue
+    size() {
+      return this.data.length
+    }
+  
+    // returns bool if queue is empty
+    isEmpty() {
+      return this.size() === 0
+    }
+  
+    // adds item to queue -- returns new size
+    enqueue(item) {
+      this.data.push(item)
+      return this.size()
+    }
+  
+    // removes item from queue -- returns new size
+    dequeue(item) {
+      this.data.shift()
+      return this.size()
+    }
+  
+    // returns the first item in the queue 
+    peek() {
+      return this.data[0]
+    }
+  
+  
+    // logs all items in the queue
+    print() {
+      this.data.forEach(element => {
+        console.log(element)
+      })
+    }
+  }
--- Stack.js
+++ Stack.js
@@ -1,1 +1,42 @@
-// Your stack class goes here
+    // Your stack class goes here
+    class Stack {
+        constructor() {
+        this.data = []
+        }
+    
+        // return the size of the stack
+        size() {
+        return this.data.length
+        }
+    
+        // returns bool if stack is empty
+        isEmpty() {
+        return this.size() == 0
+        }
+    
+        // add an item to the stack
+        push(item) {
+        this.data.push(item)
+        return this.size()
+        }
+    
+        // remove an item from the stack
+        pop() {
+        this.data.pop()
+        return this.size()
+        }
+    
+        // return the first item in the stack
+        peek() {
+        return this.data[this.data.length - 1]
+        }
+    
+        // prints all items in the stack
+        print() {
+        this.data.forEach(element => {
+            console.log(element)
+        })
+        }
+    } 
+    
+    
