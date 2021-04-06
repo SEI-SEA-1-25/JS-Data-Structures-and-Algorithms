56378464c08440fc91d3eaada4b06ecc91674db7
semuel1 <semuelst1@gmail.com>
2021-04-06 10:34:35 -0700

    hw

--- Queue.js
+++ Queue.js
@@ -1,1 +1,39 @@
 // Your Queue Class goes here
+class Queue {
+    constructor() {
+        this.data = []
+    }
+
+    // return the size of the queue
+    size() {
+        return this.data.length
+    }
+    // check if the queue is empty
+    isEmpty() {
+        return this.size() == 0
+    }
+
+    // add on item to the queue
+    enqueue(item) {
+        this.data.push(item)
+        return this.size()
+    }
+
+    // remove item from the queue
+    dequeue() {
+        this.data.shift()
+        return this.size()
+    }
+
+    // return the first item in queue but not remove it
+    peek() {
+        return this.data[0]
+    }
+
+    // print out all the items in the queue
+    print() {
+        this.data.forEach(element => {
+            console.log(element)
+        })
+    }
+}
--- bubbleSort.js
+++ bubbleSort.js
@@ -3,3 +3,15 @@ const numbers = [ 413, 445, 403, 224, 157, 312, 785, 862, 602, 354, 90, 442, 458
 
 // Your bubble sort goes here
 
+function bubbleSort(numbers) {
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = 0; j < (numbers.length - i) - 1; j++) {
+            if (numbers[j] > numbers[j + 1]) {
+                let swap = numbers[j]
+                numbers[j] = numbers[j + 1]
+                numbers[j + 1] - swap
+            }
+        }
+    }
+    console.log(numbers)
+}
