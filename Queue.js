9b9021acbba82d212d3df54cd68a9d1bbd228ddd
weston-bailey <wdbailey87@gmail.com>
2021-04-06 09:53:54 -0700

    hw code-along

--- Queue.js
+++ Queue.js
@@ -1,1 +1,43 @@
 // Your Queue Class goes here
+class Queue {
+  constructor() {
+    this.data = []
+  }
+
+  // return size of queue
+  size() {
+    return this.data.length
+  }
+
+  // isEmpty -- check if queue is empty
+  isEmpty() {
+    return this.size() == 0
+  }
+
+  // add on item to the queue 
+  enqueue(item) {
+    this.data.push(item)
+    return this.size()
+  }
+
+  // remove and item from the queue
+  // .shift() -- removes the first item from an array
+  // .unshift() -- adds to the front of the array
+  // .splice(0, 1) -- splice the 0th index only take one index
+  dequeue() {
+    this.data.shift()
+    return this.size()
+  }
+
+  // return first item in queue but not remove it
+  peek() {
+    return this.data[0]
+  }
+  
+  // print out all items in the qeueue
+  print() {
+    this.data.forEach(element => {
+      console.log(element)
+    })
+  }
+}
--- Stack.js
+++ Stack.js
@@ -1,1 +1,40 @@
 // Your stack class goes here
+class Stack {
+  constructor() {
+    this.data = []
+  }
+
+  // return size of the stack
+  size() {
+    return this.data.length
+  }
+
+  // check if the stack is empty or not -- return a bool
+  isEmpty() {
+    return this.size() == 0
+  }
+
+  // add item to stack -- return new size of the stack
+  push(item) {
+    this.data.push(item)
+    return this.size()
+  }
+
+  // remove item from stack -- always the last
+  pop() {
+    this.data.pop()
+    return this.size()
+  }
+
+  // return first item in stack without removing it
+  peek() {
+    return this.data[this.size() - 1]
+  }
+
+  // print out the stack
+  print() {
+    this.data.forEach(element => {
+      console.log(element)
+    })
+  }
+}
