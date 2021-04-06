bccf4b951efdb9cd5ac00bab8de31efdfac3bc1b
AlrightErin27 <erinmontybruce@gmail.com>
2021-04-05 19:46:43 -0700

    fin

--- Queue.js
+++ Queue.js
@@ -1,1 +1,58 @@
-// Your Queue Class goes here
+class Queue {
+  // arr to implement Q
+  constructor() {
+    this.items = [];
+  }
+  // Functions to be implemented
+  //size
+  size() {
+    return this.items.length;
+  }
+  // push(item)
+  enqueue(element) {
+    // push element into the items
+    this.items.push(element);
+    return this.size();
+  }
+  // pop()
+  dequeue() {
+    // return top most element in the queue
+    // and removes it from the queue
+    // Underflow if queue is empty
+    this.items.pop();
+    return this.size();
+  }
+  // peek()
+  peek() {
+    // return the top most element from the queue
+    // but doesn't delete it.
+    return this.items[this.items.length - 1];
+  }
+  // isEmpty()
+  isEmpty() {
+    //return if queue is empty
+    return this.items.length == 0;
+  }
+  // printQueue()
+  printQueue() {
+    // let str = "";
+    // for (let i = 0; i < this.items.length; i++) str += this.items[i] + "";
+    // return str;
+    console.log(this.items);
+  }
+}
+
+const newQueue = new Queue();
+//test to see if new Queue is empty
+// console.log(newQueue.isEmpty()); //true
+
+//test to see if pop returns "nothing to check"
+// console.log(newQueue.pop());
+
+//adding elements into Queue
+newQueue.enqueue(101);
+newQueue.enqueue(11);
+newQueue.enqueue(22);
+
+newQueue.printQueue();
+console.log(newQueue.size());
--- Stack.js
+++ Stack.js
@@ -1,1 +1,57 @@
-// Your stack class goes here
+class Stack {
+  // arr to implement stack
+  constructor() {
+    this.items = [];
+  }
+  // Functions to be implemented
+  //size
+  size() {
+    return this.items.length;
+  }
+  // push(item)
+  push(element) {
+    // push element into the items
+    this.items.push(element);
+  }
+  // pop()
+  pop() {
+    // return top most element in the stack
+    // and removes it from the stack
+    // Underflow if stack is empty
+    if (this.items.length == 0) return "Nothing to pop!";
+    return this.items.pop();
+  }
+  // peek()
+  peek() {
+    // return the top most element from the stack
+    // but doesn't delete it.
+    return this.items[this.items.length - 1];
+  }
+  // isEmpty()
+  isEmpty() {
+    //return if stack is empty
+    return this.items.length == 0;
+  }
+  // printStack()
+  printStack() {
+    // let str = "";
+    // for (let i = 0; i < this.items.length; i++) str += this.items[i] + "";
+    // return str;
+    console.log(this.items);
+  }
+}
+
+const newStack = new Stack();
+//test to see if new stack is empty
+// console.log(newStack.isEmpty()); //true
+
+//test to see if pop returns "nothing to check"
+// console.log(newStack.pop());
+
+//adding elements into stack
+newStack.push(101);
+newStack.push(11);
+newStack.push(22);
+
+newStack.printStack();
+console.log(newStack.size());
