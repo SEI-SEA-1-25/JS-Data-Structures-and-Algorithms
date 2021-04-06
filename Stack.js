19ec9f6fbce99d58894602872f921190402abb25
Jcom619 <joshuacompton@my.smccd.edu>
2021-04-06 09:09:18 -0700

    jc

--- Stack.js
+++ Stack.js
@@ -1,20 +1,24 @@
+//?-----------------------------
 class Stack {
   constructor() {
-    this.items = [];
-    this.count = 0;
-    console.clear();
-  }
+     this.data = []
+   }
+     
+  
   //?------------- PUSH ----------------
-  //! Add element to top of the stack
+  //? Add element to top of the stack
+  //?-----------------------------------
   push(element) {
     this.items[this.count] = element;
     this.count += 1;
     console.log(`${element} added to ${this.count}`);
+    console.clear();
     return this.count - 1;
-  }
+   }
+   
   //?------------ POP -----------------
-  //! return and remove top element
-  //! return undefined if stack empty
+  //? return and remove top element
+  //? return undefined if stack empty
   //?----------------------------------
   pop() {
     if (this.count == 0) return undefined;
@@ -25,33 +29,45 @@ class Stack {
   }
 
   //?---------------- PEEK -------------------
-  //!
+
   peek() {
     console.log(`top element is in ${this.items[this.count - 1]}`);
     return this.items[this.count - 1];
   }
-}
+
 //?------------------- isEmpty --------------------
-//!
+//?
+// isEmpty() {
+   
+   // }
 //?------------------------------------------------
 
-const stack = new Stack();
 
-// stack.size(1);
-// console.log(stack); // [1]
 
-stack.push(2);
-console.log(stack); // [1,2]
+   //?-----------------------------
+   const stack = () => new Stack();
+   //?-----------------------------
+   // !stack.size(1);
+   // !console.log(stack); // [1]
+   //?-----------------------------
+   stack.push(2);
+   console.log(stack); // [1,2]
+   //?-----------------------------
+   stack.peek(3);
+   console.log(stack); // [1,2,3]
+   //?-----------------------------
+   // !stack.isEmpty(4);
+   // !console.log(stack); // [1,2,3,4]
+   //?-----------------------------
+   stack.push(5);
+   console.log(stack); // [1,2,3,4,5]
+   //?-----------------------------
+   stack.pop(5);
+   console.log(stack);
+//?-----------------------------
 
-stack.peek(3);
-console.log(stack); // [1,2,3]
 
-// stack.isEmpty(4);
-// console.log(stack); // [1,2,3,4]
-
-stack.push(5);
-console.log(stack); // [1,2,3,4,5]
-
-stack.pop(5);
-console.log(stack);
-//?------------------------------------------------
+//?-----------------------------
+//?-----------------------------
+//?-----------------------------
+//?-----------------------------
